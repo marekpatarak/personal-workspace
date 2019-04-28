e612fe05cec4a491785bbe9b22f5713c3671d7ff
DOMAIN\patarak <patarak@cngroup.dk>
2019-04-28 22:15:20 +0200

    ProcurApp - add NoticeType entity

--- ProcurApp/src/main/java/com/pproject/app/control/MainController.java
+++ ProcurApp/src/main/java/com/pproject/app/control/MainController.java
@@ -2,9 +2,13 @@ package com.pproject.app.control;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.pproject.app.entity.BusinessEntity;
+import com.pproject.app.entity.NoticeType;
 import com.pproject.app.repository.BusinessEntityRepository;
+import com.pproject.app.repository.NoticeTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,9 +26,15 @@ public class MainController {
 
 	@Autowired
 	private BusinessEntityRepository businessEntityRepository;
+
+	@Autowired
+	private NoticeTypeRepository noticeTypeRepository;
 	
 	@Autowired
 	private NoticeService noticeService;
+
+	@Autowired
+	private Configuration configuration;
 
 	public static int pageLimit = 50;
 	
@@ -90,6 +100,34 @@ public class MainController {
 		return "register";
 	}
 
+	@GetMapping("/config")
+	public @ResponseBody Iterable<NoticeType>  showConfig() {
+
+		return configuration.getNoticeTypeSet();
+	}
+
+	@GetMapping("/config/update")
+	public String updateConfig() {
+
+		List<Notice> noticeList = (List<Notice>)noticeRepository.findAll();
+
+		for(Notice notice : noticeList) {
+			String noticeType = getNoticeType(notice);
+
+			if(noticeType != null && !noticeTypeRepository.existsById(noticeType)) {
+				NoticeType noticeTypeObject = new NoticeType(noticeType);
+				noticeTypeRepository.save(noticeTypeObject);
+			}
+		}
+
+		configuration.update();
+		return "success";
+
+	}
+
+
+
+
 	@PostMapping("/register")
 	public String registerSubmit(@ModelAttribute BusinessEntity businessEntity) {
 
@@ -98,4 +136,18 @@ public class MainController {
 	}
 
 
+	private String getNoticeType(Notice notice) {
+
+		String reg = "- \\w{3} :";
+		Pattern pattern = Pattern.compile(reg);
+		Matcher matcher = pattern.matcher(notice.getTitle());
+
+		if(matcher.find()) {
+			String find = matcher.group();
+			if (find.toUpperCase().equals(find)) {
+				return find.substring(2,5);
+			}
+		}
+		return null;
+	}
 }
